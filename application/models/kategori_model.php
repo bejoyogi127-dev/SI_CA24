13168ec73ae92ed4516bd1fcd3a648f772fe000d
bejoyogi127-dev <bejoyogi127@gmail.com>
2026-04-14 10:44:23 +0700

    alfin

--- application/models/kategori_model.php
+++ application/models/kategori_model.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') OR exit('No direct access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Kategori_model extends CI_Model {
+class Kategori_model extends CI_Model{
 
     private $table = 'kategori';
 
@@ -10,9 +10,24 @@ class Kategori_model extends CI_Model {
     {
         return $this->db->get($this->table)->result();
     }
+    public function get_by_id($id)
+    {
+        $this->db->where('id',$id);
+        return $this->db->get('kategori')->row();
+    }
     // Insert data
     public function insert($data)
     {
-        return $this->db->insert($this->table,$data);
+        return $this->db->insert($this->table, $data);
     }
+    public function delete($id)
+    {
+        return $this->db->delete($this->table, ['id'=>$id]);
+    }
+   public function update($id, $data)
+ {
+    $this->db->where('id', $id); // INI WAJIB
+    return $this->db->update('kategori', $data);
+}
+
 }
--- application/views/kategori/tambah.php
+++ application/views/kategori/tambah.php
@@ -3,14 +3,14 @@
 
 <div class="card shadow">
     <div class="card-body">
-<form method="post" action="<?= site_url('kategori/simpan'); ?>">
+ <form method="post" action="<?= site_url('kategori/simpan'); ?>">
     <div class="form-group">
     <label>Nama Kategori</label><br>
     <input type="text" name="nama_kategori" class="form-control" required>
 </div>
 
     <button type="submit" class="btn btn-primary">Simpan</button>
-    <a href="<? site_url('kategori');?>" class="btn btn-secondary">Kembali</a>
+    <a href="<?= site_url('kategori');?>" class="btn btn-secondary">Kembali</a>
 
 </form>
 </div>
